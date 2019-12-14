ba833f5d9d9cb5e7e6c611a2cdb4289d9f066cbb
Silvio Marques <silvio@impyco.com.br>
2019-12-14 17:44:13 -0300

    Main component implementation

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+import logo from '../../assets/logo.svg';
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
+import './style.css';
 
-export default function Main() {
+export default function Main({ match }) {
+    const [ users, setUsers ] = useState([]);
+
+    useEffect(() => {
+
+        (async function loadUsers() {
+            const response = await api.get("/devs", {
+                headers: {
+                    user: match.params.id,
+                }
+            });
+
+            setUsers(response.data);
+        })();
+
+    }, [match.params.id]);
+
     return (
-        <h1>Main</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                {users.map(user => {
+                    function showBio() {
+                        if (user.bio) {
+                            return user.bio;
+                        } else {
+                            return "No description provided.";
+                        }
+                    }
+                    return (
+                        <li key={user._id}>
+                            <img src={user.avatar} alt={user.name} />
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{showBio()}</p>
+                            </footer>
+                            <div className="buttons">
+                                <button type="button" className="like">
+                                    <img src={like} alt="Like" />
+                                </button>
+                                <button type="button" className="dislike">
+                                    <img src={dislike} alt="Dislike" />
+                                </button>
+                            </div>
+                        </li>
+                    )
+                }
+                )}
+            </ul>
+        </div>
     );
 }
